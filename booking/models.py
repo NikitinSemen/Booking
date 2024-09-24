e7e7b0a7d317ed9d481de752a501ca3be91f15e8
semen <pr.pypblrka@gmail.com>
2024-09-24 10:58:08 +0400

    добавлены модели

--- booking/models.py
+++ booking/models.py
@@ -1,2 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Table(models.Model):
+    number = models.IntegerField(unique=True)
+    capacity = models.IntegerField()
+
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    guests = models.IntegerField()
+    confirmed = models.BooleanField(default=False)
+
+
 
--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="укажите почту"
+    )
+    phone = models.CharField(max_length=35, **NULLABLE, verbose_name="номер телефона")
+    tg_id = models.CharField(max_length=100, verbose_name="Телеграм ID", **NULLABLE)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"{self.email}"
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
